d0cac3826c7029b3412131666174fd12b7b8f6e2
spencer-shadley <spencershadley@utexas.edu>
2014-10-09 19:04:56 -0500

    pseudo

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -17,4 +17,32 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+
+  while(f->eip != null)
+  {
+
+	  // get the error code (which system call)
+  	uint32_t error_code = f->error_code;
+
+  	// redirect to the correct function handler
+  	switch(error_code)
+  	{
+  		case wait: wait();
+  			break;
+  		case exec: exec();
+  			break;
+  		default:
+  			printf("uh oh");
+  			break;
+  		// ...
+  	}
+  	
+  }
+
+}
+
+void
+wait()
+{
+	// handle wait
 }
